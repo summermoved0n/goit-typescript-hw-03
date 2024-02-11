5a74698d6b8cee144de5b981bd1e7bceb5c275ce
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-02-11 01:57:08 -0500

    hw3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+class Key {
+  constructor(private signature: number = Math.random()) {
+    this.signature = signature;
+  }
+
+  getSignature(): number {
+    console.log("My key ", this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered in house.");
+    } else {
+      console.log("The door is closed!");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Wrong key.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +65,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
